cad0193350accce0a0bd82330fd15f726e85aaf8
Adrien RAIMBAULT <adrien-raimbault@protonmail.com>
2022-07-12 12:52:22 +0200

    clean

--- functions/requeteBDD.php
+++ functions/requeteBDD.php
@@ -1,15 +1,14 @@
 <?php
-function recupRole(){
-try{
+function recupRole()
+{
+    try {
         $conn = getPdo();
         $requete = $conn->prepare("SELECT * FROM role ");
         $requete->execute();
         $roles = $requete->fetchAll(PDO::FETCH_ASSOC);
-
+    } catch (PDOException $e) {
+        die("Erreur : " . $e->getMessage());
     }
-    catch(PDOException $e){
-    die("Erreur : " . $e->getMessage());
-    }
-    $conn= null;
-return $roles;
+    $conn = null;
+    return $roles;
 }
