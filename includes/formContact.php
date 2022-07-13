7e1262f5c282e306d9c64bd58e851bf06133dc14
Adrien RAIMBAULT <adrien-raimbault@protonmail.com>
2022-07-13 14:07:27 +0200

    contact style form

--- includes/formContact.php
+++ includes/formContact.php
@@ -1,13 +1,13 @@
 <div id="formulaire">
     <form method="post" action="index.php?page=home#contact">
         <ul>
-            <li><input placeholder="Nom" type="text" id="nom" name="nom" value="<?php echo $nom; ?>" /></li>
+            <li><input placeholder="* Nom" minlength="3" type="text" id="nom" name="nom" value="<?php echo $nom; ?>" required /></li>
+
+            <li><input placeholder="* E-mail" type="email" id="mail" name="mail" value="<?php echo $mail; ?>" required /></li>
 
             <li><input placeholder="Prénom" type="text" id="prenom" name="prenom" value="<?php echo $prenom; ?>" /></li>
 
-            <li><input placeholder="Télephone" type="tel" id="telephone" name="telephone" value="<?php echo $telephone; ?>" /></li>
-
-            <li><input placeholder="E-mail" type="text" id="mail" name="mail" value="<?php echo $mail; ?>" /></li>
+            <li><input placeholder="Téléphone" type="tel" id="telephone" name="telephone" value="<?php echo $telephone; ?>" /></li>
 
             <li><textarea placeholder="Objet" id="objet" name="objet" value="<?php echo $objet; ?>"></textarea></li>
 
